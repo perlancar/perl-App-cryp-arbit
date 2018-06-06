599ef31d8149ee7ab7c0d91ee85d5f2cd5c51820
Steven Haryanto (on pc-office) <stevenharyanto@gmail.com>
2018-06-06 23:18:44 +0700

    Finish refactoring, now can produce order pairs again

--- lib/App/cryp/arbit.pm
+++ lib/App/cryp/arbit.pm
@@ -44,29 +44,37 @@ our $db_schema_spec = {
         # XXX later move to cryp-folio?
         'CREATE TABLE exchange (
              id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-             code VARCHAR(8) NOT NULL, UNIQUE(code),
              safename VARCHAR(100) NOT NULL, UNIQUE(safename)
          )',
 
         # XXX later move to cryp-folio?
         'CREATE TABLE account (
              id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-             ctime DOUBLE NOT NULL,
              exchange_id INT NOT NULL,
+             nickname VARCHAR(64) NOT NULL,
+             UNIQUE(exchange_id,nickname),
              note VARCHAR(255)
          )',
 
         # XXX later move to cryp-folio?
-        'CREATE TABLE balance (
+        'CREATE TABLE latest_balance (
              id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-             time DOUBLE NOT NULL, INDEX(time),
+             time DOUBLE NOT NULL,
              account_id INT NOT NULL,
              currency VARCHAR(10) NOT NULL,
-             amount DECIMAL(21,8) NOT NULL,
-             note VARCHAR(255)
+             UNIQUE(account_id, currency),
+             available DECIMAL(21,8) NOT NULL
          )',
 
-        # XXX create balance_Log? (later move to cryp-folio)?
+        # XXX later move to cryp-folio?
+        'CREATE TABLE balance_history (
+             id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+             time DOUBLE NOT NULL,
+             account_id INT NOT NULL,
+             currency VARCHAR(10) NOT NULL,
+             UNIQUE(time, account_id, currency),
+             available DECIMAL(21,8) NOT NULL
+         )',
 
         # XXX later move to cryp-folio?
         'CREATE TABLE tx (
@@ -81,7 +89,8 @@ our $db_schema_spec = {
              time DOUBLE NOT NULL, INDEX(time),
              currency1 VARCHAR(10) NOT NULL,
              currency2 VARCHAR(10) NOT NULL,
-             price DECIMAL(21,8) NOT NULL, -- price to buy currency1 in currency2, e.g. currency1 = BTC, currency2 = USD, price = 11150
+             type VARCHAR(4) NOT NULL,
+             price DECIMAL(21,8) NOT NULL, -- price to buy (or sell) currency1 in currency2, e.g. currency1 = BTC, currency2 = USD, price = 11150 means 1 BTC is $11150
              exchange_id INT NOT NULL,
              note VARCHAR(255)
          )',
@@ -142,30 +151,127 @@ sub _exchange_catalog {
 sub _get_exchange_id {
     my ($r, $exchange) = @_;
 
+    return $r->{_stash}{exchange_ids}{$exchange} if
+        $r->{_stash}{exchange_ids}{$exchange};
+
     my $xcat = _exchange_catalog();
     my $rec = $xcat->by_safename($exchange);
     $rec or die "BUG: Unknown exchange '$exchange'";
 
     my $dbh = $r->{_stash}{dbh};
 
-    my ($id) = $dbh->selectrow_array("SELECT id FROM exchange WHERE safename=?", {}, $exchange);
-
-    unless ($id) {
-        $dbh->do("INSERT INTO exchange (code,safename) VALUES (?,?)", {}, $rec->{code}, $exchange);
-        $id = $dbh->last_insert_id("","","","");
+    my ($eid) = $dbh->selectrow_array("SELECT id FROM exchange WHERE safename=?", {}, $exchange);
+    unless ($eid) {
+        $dbh->do("INSERT INTO exchange (safename) VALUES (?)", {},
+                 $exchange);
+        $eid = $dbh->last_insert_id("","","","");
     }
 
-    $id;
+    $r->{_stash}{exchange_ids}{$exchange} = $eid;
+    $eid;
+}
+
+sub _get_account_id {
+    my ($r, $exchange, $account) = @_;
+
+    return $r->{_stash}{account_ids}{$exchange}{$account} if
+        $r->{_stash}{account_ids}{$exchange}{$account};
+
+    my $dbh = $r->{_stash}{dbh};
+
+    my $eid = _get_exchange_id($r, $exchange);
+
+    my ($aid) = $dbh->selectrow_array("SELECT id FROM account WHERE exchange_id=? AND nickname=?", {}, $eid, $account);
+    unless ($aid) {
+        $dbh->do("INSERT INTO account (exchange_id,nickname) VALUES (?,?)", {}, $eid, $account);
+        $aid = $dbh->last_insert_id("","","","");
+    }
+
+    $r->{_stash}{account_ids}{$exchange}{$account} = $aid;
+    $aid;
+}
+
+sub _get_account_balances {
+    my $r = shift;
+
+    my $dbh = $r->{_stash}{dbh};
+    $r->{_stash}{account_balances} = {};
+
+    for my $e (sort keys %{ $r->{_stash}{exchange_clients} }) {
+        my $clients = $r->{_stash}{exchange_clients}{$e};
+      ACC:
+        for my $acc (sort keys %$clients) {
+            my $aid = _get_account_id($r, $e, $acc);
+            my $client = $clients->{$acc};
+            my $time = time();
+            my $res = $client->list_balances;
+            unless ($res->[0] == 200) {
+                log_error "Couldn't list balances for account %s/%s: %s, skipping account",
+                    $e, $acc, $res;
+                next ACC;
+            }
+            $r->{_stash}{account_balances}{$e}{$a} = $res->[2];
+            for my $rec (@{ $res->[2] }) {
+                $dbh->do(
+                    "REPLACE INTO latest_balance (time, account_id, currency, available) VALUES (?,?,?,?)",
+                    {},
+                    $time, $aid, $rec->{currency}, $rec->{available},
+                );
+                $dbh->do(
+                    "INSERT INTO balance_history (time, account_id, currency, available) VALUES (?,?,?,?)",
+                    {},
+                    $time, $aid, $rec->{currency}, $rec->{available},
+                );
+            } # for rec
+        } # for account
+    } # for exchange
+
+    $r->{_stash}{account_balances};
+}
+
+sub _get_exchange_pairs {
+    my ($r, $exchange) = @_;
+
+    return $r->{_stash}{exchange_pairs}{$exchange} if
+        $r->{_stash}{exchange_pairs}{$exchange};
+
+    my $clients = $r->{_stash}{exchange_clients}{$exchange};
+    my $client = $clients->{ (sort keys %$clients)[0] };
+
+    my $res = $client->list_pairs;
+    if ($res->[0] == 200) {
+        $r->{_stash}{exchange_pairs}{$exchange} = $res->[2];
+    } else {
+        log_error "Couldn't list pairs on %s: %s, ".
+            "skipping this exchange", $exchange, $res;
+        $r->{_stash}{exchange_pairs}{$exchange} = [];
+    }
+
+    $r->{_stash}{exchange_pairs}{$exchange};
+}
+
+sub _get_trading_fee {
+    my ($r, $exchange, $coin) = @_;
+
+    my $fees = $r->{_stash}{trading_fees};
+    my $fees_exchange = $fees->{$exchange} // $fees->{':default'};
+    my $fee = $fees_exchange->{$coin} // $fees_exchange->{':default'};
+}
+
+sub _is_fiat {
+    require Locale::Codes::Currency_Codes;
+    no warnings 'once';
+    my $code = shift;
+    $Locale::Codes::Data{'currency'}{'code2id'}{alpha}{uc $code} ? 1:0;
 }
 
 sub _init {
     my $r = shift;
 
-    my %acc_exchanges; # key = exchange safename, value = {account1=>1, account2=>1, ...)
+    my %account_exchanges; # key = exchange safename, value = {account1=>1, account2=>1, ...)
 
     my $xcat = _exchange_catalog();
 
-    # check arguments
   CHECK_ARGUMENTS:
     {
         # there must be at least two accounts on two different exchanges
@@ -174,20 +280,20 @@ sub _init {
         for (@{ $r->{args}{accounts} }) {
             m!(.+)/(.+)! or return [400, "Invalid account '$_', please use EXCHANGE/ACCOUNT syntax"];
             my ($xchg, $acc) = ($1, $2);
-            unless (exists $acc_exchanges{$xchg}) {
+            unless (exists $account_exchanges{$xchg}) {
                 my $rec = $xcat->by_safename($xchg);
                 return [400, "Unknown exchange '$xchg'"] unless $rec;
                 return [400, "Exchange '$xchg' is not assigned short code yet. ".
                             "please contact the maintainer of CryptoExchange::Catalog ".
                             "to add one for it"] unless $rec->{code};
             }
-            $acc_exchanges{$xchg}{$acc} = 1;
+            $account_exchanges{$xchg}{$acc} = 1;
         }
         return [400, "Please specify accounts on at least two ".
                     "cryptoexchanges, you only specify account(s) on " .
-                    join(", ", keys %acc_exchanges)]
-            unless keys(%acc_exchanges) >= 2;
-        $r->{_stash}{acc_exchanges} = \%acc_exchanges;
+                    join(", ", keys %account_exchanges)]
+            unless keys(%account_exchanges) >= 2;
+        $r->{_stash}{account_exchanges} = \%account_exchanges;
     }
 
     my $dbh;
@@ -218,18 +324,20 @@ sub _init {
     {
         my $time = time();
 
-        for my $exchange (sort keys %acc_exchanges) {
+        for my $exchange (sort keys %account_exchanges) {
 
             my $mod = "App::cryp::Exchange::$exchange";
             $mod =~ s/-/_/g;
             (my $modpm = "$mod.pm") =~ s!::!/!g;
             require $modpm;
 
-            my $accounts = $acc_exchanges{$exchange};
+            my $accounts = $account_exchanges{$exchange};
             for my $account (sort keys %$accounts) {
                 # assign an ID to the exchange, if not already so
                 my $eid = _get_exchange_id($r, $exchange);
-                $r->{_stash}{exchange_ids}{$exchange} = $eid;
+                # assign an ID to the account, if not already so
+                my $aid = _get_account_id($r, $exchange, $account);
+                $r->{_stash}{exchange_recs}{$exchange} = $xcat->by_safename($exchange);
 
                 unless ($r->{_cryp}{exchanges}{$exchange}{$account}) {
                     return [400, "No configuration found for exchange $exchange (account $account). ".
@@ -243,9 +351,90 @@ sub _init {
         } # exchange
     } # INSTANTIATE_EXCHANGE_CLIENTS
 
-    use DD; dd $r->{_stash};
+    [200];
+}
+
+sub _init_arbit {
+    my $r = shift;
+
+    my @coins;
+    my @coins0 = @{ $r->{args}{coins} // [] };
+    my %coin_exchanges; # key=cryptocurrency code, value={exchange1=>1, ...}
+
+  DETERMINE_COINS_TO_ARBITRAGE:
+    {
+        # list pairs on all exchanges
+        for my $e (sort keys %{ $r->{_stash}{exchange_clients} }) {
+            my $clients = $r->{_stash}{exchange_clients}{$e};
+            # pick first account
+            my $acc = (sort keys %$clients)[0];
+            my $client = $clients->{$acc};
+            my $pairs = _get_exchange_pairs($r, $e);
+            # XXX for now, only consider CRYPTO/FIAT pair
+            for my $pair (@$pairs) {
+                my ($cur1, $cur2) = split m!/!, $pair;
+                next unless _is_fiat($cur2);
+                if ($r->{args}{fiats}) {
+                    next unless grep { $_ eq $cur2 } @{ $r->{args}{fiats} };
+                }
+                $coin_exchanges{$cur1}{$e} = 1;
+            }
+        }
+
+        # only consider coins that are traded in >1 exchanges, for
+        # arbitrage possibility
+        my @possible_coins = grep { keys(%{$coin_exchanges{$_}}) > 1 }
+            keys %coin_exchanges;
+
+        if (@coins0) {
+            my @impossible_coins;
+            my @coins1;
+            for my $c (@coins0) {
+                if (grep { $c eq $_ } @possible_coins) {
+                    push @coins1, $c;
+                } else {
+                    push @impossible_coins, $c;
+                }
+            }
+            if (@impossible_coins) {
+                log_warn "The following coin(s) are not traded on at least two exchanges: %s, excluding these coins",
+                    \@impossible_coins;
+                @coins = @coins1;
+            }
+        } else {
+            log_warn "Will be arbitraging these coin(s) that are traded on at least two exchanges: %s",
+                \@possible_coins;
+            @coins = @possible_coins;
+        }
+
+        return [412, "No coins possible for arbitraging"] unless @coins;
+        $r->{_stash}{coins} = \@coins;
+    } # DETERMINE_COINS_TO_ARBITRAGE
+
+  DETERMINE_TRADING_FEES:
+    {
+        # XXX hardcoded for now
+        $r->{_stash}{trading_fees} = {
+            ':default' => {':default'=>0.3},
+            'indodax'  => {':default'=>0.3},
+            'gdax'     => {BTC=>0.25, ':default'=>0.3},
+        };
+    }
 
     [200];
+}
+
+$SPEC{dump_cryp_config} = {
+    v => 1.1,
+    args => {
+    },
+};
+sub dump_cryp_config {
+    my %args = @_;
+
+    my $r = $args{-cmdline_r};
+
+    [200, "OK", $r->{_cryp}];
 }
 
 $SPEC{arbit} = {
@@ -285,9 +474,29 @@ cryptoexchange.
 
 _
         },
-        check_prices_frequency => {
+        coins => {
+            summary => 'Cryptocurrencies to arbitrate',
+            schema => ['array*', of=>'cryptocurrency*', min_len=>1],
+            description => <<'_',
+
+If not specified, will list all supported pairs on all the exchanges and include
+the cryptocurrencies that are listed on at least 2 different exchanges (for
+arbitrage possibility).
+
+_
+        },
+        fiats => {
+            summary => 'Fiat currencies to arbitrate',
+            schema => ['array*', of=>'fiat_currency*', min_len=>1],
+            description => <<'_',
+
+If not specified, will allow any fiat currencies.
+
+_
+        },
+        arbit_frequency => {
             summary => 'How many seconds to wait between checking prices '.
-                'at the cryptoexchanges (in seconds)',
+                'and creating arbitration orders (in seconds)',
             schema => 'posint*',
             default => 30,
             tags => ['category:timing'],
@@ -365,10 +574,13 @@ sub arbit {
     my %args = @_;
 
     my $r = $args{-cmdline_r};
-    my $res = _init($r); return $res unless $res->[0] == 200;
-
     # XXX schema
     my $strategy = $args{strategy} // 'merge_order_book';
+
+    my $res;
+
+    $res = _init($r); return $res unless $res->[0] == 200;
+    $res = _init_arbit($r); return $res unless $res->[0] == 200;
 
     log_info "Starting arbitration with '%s' strategy ...", $strategy;
 
@@ -376,10 +588,39 @@ sub arbit {
     (my $strategy_modpm = "$strategy_mod.pm") =~ s!::!/!g;
     require $strategy_modpm;
 
+  ROUND:
     while (1) {
-        _check_prices($r);
-        log_trace "Sleeping %d second(s) ...", $args{check_prices_frequency};
-        sleep $args{check_prices_frequency};
+        $res = $strategy_mod->create_order_pairs(r => $r);
+
+        if ($res->[0] == 200) {
+            log_debug "Got these orders from arbit strategy module: %s",
+                $res->[2];
+        } else {
+            log_error "Got error response from arbit strategy module: %s, ".
+                "skipping this round", $res;
+            goto SLEEP;
+        }
+
+        if ($args{-dry_run}) {
+            log_info "[DRY-RUN] ";
+            goto SLEEP;
+        }
+
+        #$res = _submit_orders($r, $res->[2]);
+        #unless ($res->[0] == 200) {
+        #    log_error "Got error response when submitting orders: %s", $res;
+        #}
+
+        #$res = _expire_submitted_orders($r);
+        #unless ($res->[0] == 200) {
+        #    log_error "Got error response when expiring submitted orders: %s",
+        #        $res;
+        #}
+
+      SLEEP:
+        log_trace "Sleeping for %d second(s) before next round ...",
+            $args{arbit_frequency};
+        sleep $args{arbit_frequency};
     }
 
     [200]; # should not be reached
@@ -413,9 +654,16 @@ passing C<$r> around. The keys that are used by routines in this module:
 
  $r->{_stash}
    {dbh}
-   {acc_exchanges}     # key=exchange safename, value={account1 => 1, ...}
+   {account_balances}  # key=exchange safename, value={account1 => [{currency=>CUR1, available=>..., ...}, {...}], ...}
+   {account_exchanges} # key=exchange safename, value={account1 => 1, ...}
+   {account_ids}       # key=exchange safename, value={account1 => numeric ID from db, ...}
+   {coins}             # coins to arbitrage
    {exchange_clients}  # key=exchange safename, value={account1 => $client1, ...}
    {exchange_ids}      # key=exchange safename, value=exchange (numeric) ID from db
+   {exchange_recs}     # key=exchange safename, value=hash (from CryptoExchange::Catalog)
+   {exchange_coins}    # key=exchange safename, value=[COIN1, COIN2, ...]
+   {exchange_pairs}    # key=exchange safename, value=[PAIR1, PAIR2, ...]
+   {trading_fees}      # key=exchange safename, value={coin1=>num (in percent) market taker fees, ...}, ':default' for all other coins, ':default' for all other exchanges
 
 To be cleaner and more documented, when communicating with routines in other
 modules (including C<App::cryp::Arbit::Strategy::*> modules), we use standard
